5a65cbd8b4333de4ad946d06ef551a04a2807c46
Robert Nagtegaal <masikh@gmail.com>
2022-01-31 00:31:26 +0100

    Create reboot.c

---
+++ reboot.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/reboot.h>
+
+
+int
+main (int argc, char **argv)
+{
+	if (reboot (RB_AUTOBOOT) < 0) {
+		fprintf (stderr, "%s: call to reboot() system call failed.\n",
+			 argv[0]);
+	}
+
+	return 0;
+}
